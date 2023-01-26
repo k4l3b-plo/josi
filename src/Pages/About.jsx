9b770510a33ed0e7eb0ab0ad3036414ed7465469
CalebBlion <kaleba647@gmail.com>
2023-01-26 05:44:57 +0300

    finish

---
+++ src/Pages/About.jsx
@@ -0,0 +1,12 @@
+const About = () => {
+    return (
+        <div>
+            <h1>Project built by</h1>
+            <p>Yosef tesfaye: uu81139R</p>
+            <p>Nphtalem Balcha: UU80784R</p>
+            <p>Natnael Selomon: uu81000R</p>
+        </div>
+    )
+}
+
+export default About
